3aa554fed344a0993e0bcd78587da47dffd87381
paolaespinosa2003 <paolaespinosa2c@gmail.com>
2025-08-30 18:14:42 -0600

    Update AbrirEnlace.cs

--- 2.1. La primera ventana/Assets/Scripts/AbrirEnlace.cs
+++ 2.1. La primera ventana/Assets/Scripts/AbrirEnlace.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class AbrirEnlace : MonoBehaviour
 {
-    public void AbrirURL(string url)
+    public void AbrirGoogle()
     {
-        Application.OpenURL(url);
+        Application.OpenURL("https://www.google.com");
+    }
+
+    public void AbrirYouTube()
+    {
+        Application.OpenURL("https://www.youtube.com");
+    }
+
+    public void AbrirWikipedia()
+    {
+        Application.OpenURL("https://www.wikipedia.org");
     }
 }
+
+
